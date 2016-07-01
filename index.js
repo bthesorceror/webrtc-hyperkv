f4d03c8d23a6964f1488e3bf9636cfe80682cca0
bthesorceror <bthesorceror@gmail.com>
2016-06-30 23:10:15 -0600

    Clean up server code

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
 
-const path = require('path');
-const http = require('http');
+const path     = require('path');
+const http     = require('http');
 const ecstatic = require('ecstatic');
 
-http.createServer(ecstatic({
-  root: path.join(__dirname, 'static')
-})).listen(process.env.PORT || 8080)
+const port             = process.env.PORT || 8080;
+const staticPath       = path.join(__dirname, 'static');
+const staticMiddleware = ecstatic({ root: staticPath });
+
+function onListen() {
+  console.info(`Server now running on port ${port}`);
+}
+
+function onRequest(req, res) {
+  console.info(`${req.method} ${req.url}`);
+}
+
+http
+  .createServer(staticMiddleware)
+  .on('request', onRequest)
+  .listen(port, onListen);
